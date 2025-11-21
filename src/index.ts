a025581722da4ced75fcc3308fd832f26e3a39ec
tvhfrks12678 <tvhfrks12678@gmail.com>
2025-11-21 11:39:19 +0900

    learn: At a glance

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+
+import { Console, Effect } from "effect/index";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+new Elysia()
+  .get('/', 'Hello Elysia !!!')
+  .get('/user/:id', ({ params: { id }}) => id)
+  .post('/form', ({ body }) => body)
+  .listen(3000)
+
+// console.log(
+//   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+// );
+
+const program = Console.log("Hello, World! effect-ts");
+
+Effect.runSync(program)
